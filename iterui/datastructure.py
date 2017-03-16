fd7347923ecbf338bf0415f17ca4e23de96dd1a8
theysp <kylin0011@163.com>
2017-03-17 00:25:36 +0800

    Modify data structure
    Modify data structure

--- iterui/datastructure.py
+++ iterui/datastructure.py
@@ -8,46 +8,60 @@
 
 import numpy as np
 
-class ElementPool:
-    def __init__(self):
-
-
-class Element:
-    def __init__(self,name):
-        self._name = name;
-        self.AllActivationData
-
-
-class Material:
-    def __init__(self):
-        self._elements = {}
-        self._activationdata = AllActivationData()
-
-    def __init__(self, elements):
-        self._elements = elements
-
-    def AddElement(self,elementName,proportion):
-        if("elementName" in self.elments):
-            self._elements[elementName] = self._elements[elementName] + proportion
-        else:
-            self._elements[elementName] = proportion
-
-    def CalculateActivation(self):
-        propSum = 0.0
-        for name,prop in self._elements.items():
-            propSum += prop
-        for name,prop in self._elements.items():
-            propSum += prop
-
 class AllActivationData:
     def __init__(self):
-        self._activity = []
-        self._
+        #6 activity, dose rate, heat, ingestion dose, #6 spectrums
+        self.activity = []
+        self.dose = []
+        self.heat = []
+        self.idose = []
+        self.spectrum = []
+
     def __init__(self,activationFileName):
 
     def LoadFromFile(self,activationFileName):
         #读取活化数据
         #读取核素演变数据
+
+
+class Material:
+    def __init__(self):
+        self.elements = {}
+        self.activationdata = AllActivationData()
+
+    def __init__(self, ielements):
+        self.elements = ielements
+
+    def AddElement(self,elementName,proportion):
+        if("elementName" in self.elements):
+            self.elements[elementName] = self.elements[elementName] + proportion
+        else:
+            self.elements[elementName] = proportion
+
+    def CalculateActivation(self):
+        propSum = 0.0
+        for name,prop in self.elements.items():
+            propSum += prop
+        for name,prop in self.elements.items():
+            propSum += prop
+
+class Element(Material):
+    def __init__(self,name):
+        super(Element,self).__init__(self)
+        self.elements[name] = 1.0;
+        if(not self.AllActivationData.LoadFromFile(name+".out")):
+            self.valid = False
+        else:
+            self.valid = True
+
+class ElementPool:
+    def __init__(self):
+        self.elementPool = {}
+
+    def GetElement(self,name):
+        if(name in self.elementPool):
+            return self.elementPool[name]
+        self.elementPool[name] = Element(name)
 
 class PathWayAnalysis:
     def __init__(self):
