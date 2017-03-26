68e785f50b58197cf4cdfd0bbaafb85726e3b0ea
theysp <kylin0011@163.com>
2017-03-26 13:34:26 +0800

    add data reading codes

--- iterui/data_handling/activationdata.py
+++ iterui/data_handling/activationdata.py
@@ -2,9 +2,20 @@
 
 
 import numpy as np
-
+import re
 import copy
 
+def eval_str_number(num_str):
+    new_num_str = num_str
+    pos_minus = num_str.find('-')
+    if num_str.find('-') > 0:
+        ch_before_minus = num_str[pos_minus-1]
+        if ch_before_minus != 'e' or ch_before_minus != 'E' :
+            new_num_str = num_str[:pos_minus] + 'E' + num_str[pos_minus:]
+    try:
+        return eval(new_num_str)
+    except Exception:
+        return -1.0
 
 class ActivationData:
     def __init__(self):
@@ -12,7 +23,7 @@ class ActivationData:
         self.activition_data = []
 
     def __init__(self,fispact_output):
-        self.load_file(fispact_output)
+        self.read_raw_file(fispact_output)
 
     def read_raw_file(self,matname):
         #obtain 5 files
@@ -67,7 +78,10 @@ class OneSpectrumActivationData:
             #if need to skip previous time steps, need to change the range
             for i in range(1, len(startlinesforsteps)):
                 onestepdata = OneSpectrumOneStepActivationData()
-                onestepdata.load_from_raw_lines(lines, startlinesforsteps[i-1], startlinesforsteps[i])
+                try:
+                    onestepdata.load_from_raw_lines(lines, startlinesforsteps[i-1], startlinesforsteps[i])
+                except Exception as err:
+                    print(err.args)
                 self.allStepsActivationData.append(onestepdata)
         finally:
             return opensucessful
@@ -85,12 +99,13 @@ class OneSpectrumOneStepActivationData:
                    'cur_mass(kg)',
                    'neutron_flux(n/cm**2/s)',
                    'number_fission',
-                   'dose_rate(Sv)',
-                   'ingestion_dose(Sv)',
-                   'inhalation_dose(Sv)',
-                   'ingestion_dose_ex_tritium(Sv)',
-                   'inhalation_dose_ex_tritium(Sv)',
+                   'dose_rate(Sv/kg)',
+                   'ingestion_dose(Sv/kg)',
+                   'inhalation_dose(Sv/kg)',
+                   'ingestion_dose_ex_tritium(Sv/kg)',
+                   'inhalation_dose_ex_tritium(Sv/kg)',
                    'gase_rate(appm/sec)']
+    num_pattern = re.compile('\d+\.?\d+E?{+-}?\d+')
     def __init__(self):
         self.time = 0.0
         self.nuclides = dict() #核素以及原子数比
@@ -98,6 +113,7 @@ class OneSpectrumOneStepActivationData:
         self.gamma_erg_bin = []
         self.gamma_spectra_cc = []
         self.gamma_spectra_power = []
+        self.ok = False
 
     def load_from_raw_lines(self, lines, startidx, endidx):
         # nuclides info (first)
@@ -105,16 +121,78 @@ class OneSpectrumOneStepActivationData:
         nuclide_data_end_idx = nuclide_data_start_idx
         for i in range(nuclide_data_start_idx, endidx):
             if lines[i].startswith('0'):
-                nuclide_data_end_idx = i
+                nuclide_data_end_idx = i-1
                 break
         for i in range(nuclide_data_start_idx, nuclide_data_end_idx):
             one_nuclide_data = OneNuclideData()
             if one_nuclide_data.read_raw_line(lines[i]):
                 self.nuclides[one_nuclide_data.nuclide_name] = one_nuclide_data
-        # total info
-
+     # total info
+        # 'total_activity(Bq)'
+        self.parameters['total_activity(Bq)'] = eval_str_number(lines[nuclide_data_end_idx + 6][40:51])
+        # 'total_activity_no_tritium(Bq)'
+        self.parameters['total_activity_no_tritium(Bq)'] = eval_str_number(lines[nuclide_data_end_idx + 7][40:51])
+        # 'alpha_heat(kW)'
+        self.parameters['alpha_heat(kW)'] = eval_str_number(lines[nuclide_data_end_idx + 8][40:51])
+        # 'beta_heat(kW)'
+        self.parameters['beta_heat(kW)'] = eval_str_number(lines[nuclide_data_end_idx + 9][40:51])
+        # 'gamma_heat(kW)'
+        self.parameters['gamma_heat(kW)'] = eval_str_number(lines[nuclide_data_end_idx + 10][40:51])
+        # 'total_heat(kW)'
+        self.parameters['total_heat(kW)'] = eval_str_number(lines[nuclide_data_end_idx + 10][90:101])
+        # 'total_heat_ex_tritium(kW)'
+        self.parameters['total_heat_ex_tritium(kW)'] = eval_str_number(lines[nuclide_data_end_idx + 11][90:101])
+        # 'origin_mass(kg)'
+        self.parameters['origin_mass(kg)'] = eval_str_number(lines[nuclide_data_end_idx + 11][40:51])
+        # 'cur_mass(kg)'
+        self.parameters['cur_mass(kg)'] = eval_str_number(lines[nuclide_data_end_idx + 12][40:51])
+        # 'neutron_flux(n/cm**2/s)'
+        self.parameters['neutron_flux(n/cm**2/s)'] = eval_str_number(lines[nuclide_data_end_idx + 13][40:51])
+        # 'number_fission'
+        self.parameters['number_fission'] = eval_str_number(lines[nuclide_data_end_idx + 14][40:51])
+        # 'ingestion_dose(Sv/kg)'
+        self.parameters['ingestion_dose(Sv/kg)'] = eval_str_number(lines[nuclide_data_end_idx + 15][40:51])
+        # 'inhalation_dose(Sv/kg)'
+        self.parameters['inhalation_dose(Sv/kg)'] = eval_str_number(lines[nuclide_data_end_idx + 16][40:51])
+        # 'ingestion_dose_ex_tritium(Sv/kg)'
+        self.parameters['ingestion_dose_ex_tritium(Sv/kg)'] = eval_str_number(lines[nuclide_data_end_idx + 17][40:51])
+        # 'inhalation_dose_ex_tritium(Sv/kg)'
+        self.parameters['inhalation_dose_ex_tritium(Sv/kg)'] = eval_str_number(lines[nuclide_data_end_idx + 18][40:51])
+        # 'gase_rate(appm/sec)'
+        self.parameters['inhalation_dose_ex_tritium(Sv/kg)'] = eval_str_number(lines[nuclide_data_end_idx + 19][27:38])
         # spectra
-
+        spectra_beg_idx = -1
+        spectra_end_idx = -1
+        for i in range(nuclide_data_end_idx + 20, endidx):
+            if lines[i].find('GAMMA SPECTRUM AND ENERGIES/SECOND') >0:
+                spectra_beg_idx = i+7
+                break
+        if spectra_beg_idx < 0:
+            raise Exception('cannot find spectra')
+        for i in range(spectra_beg_idx, endidx):
+            if lines[i].find('(') < 0:
+                spectra_end_idx = i-1
+                break
+        if spectra_end_idx < 0:
+            raise Exception('spectra format error, no empty line after spectra')
+        for i in range(spectra_beg_idx, spectra_end_idx):
+            (erg, gamma_power, gammas) = (eval_str_number(lines[i][65:69]),
+                                          eval_str_number(lines[i][77:88]),
+                                          eval_str_number(lines[i][130:141]))
+            self.gamma_erg_bin.append(erg)
+            self.gamma_spectra_cc.append(gamma_power)
+            self.gamma_spectra_power.append(gammas)
+        # 'dose_rate(Sv/kg)'
+        dose_rate_idx = -1
+        for i in range(spectra_end_idx, endidx):
+            if lines[i].find('DOSE RATE (PLANE SOURCE) FROM GAMMAS WITH ENERGY') >0:
+                dose_rate_idx = i
+                break
+        if dose_rate_idx <0:
+            self.parameters['dose_rate(Sv/kg)'] = -1.0
+        else:
+            self.parameters['dose_rate(Sv/kg)'] = eval_str_number(lines[dose_rate_idx][75:88])
+        self.ok = True
         return True
 
 
@@ -144,11 +222,11 @@ class OneNuclideData:
             return False
         self.nuclide_name = val_list[0]+val_list[1]
         for i in range(0, len(OneNuclideData.param_names)-1):
-            self.params[OneNuclideData.param_names[i]] = eval(val_list[i])
+            self.params[OneNuclideData.param_names[i]] = eval_str_number(val_list[i])
         if val_list[-1] == 'Stable':
             self.params['half_life(sec)'] = -1
         else:
-            self.params['half_life(sec)'] = eval(val_list[-1])
+            self.params['half_life(sec)'] = eval_str_number(val_list[-1])
         return True
 
 
@@ -186,3 +264,7 @@ class PathWay:
         new_path_way = copy.deepcopy(self)
         new_path_way += other_pathway
         return new_path_way
+
+if __name__ == '__main__':
+    data_test = OneSpectrumActivationData()
+    data_test.read_raw_file('C:/Users/ysp/Desktop/QT_practice/ITER DATA/Flux2/B/B.out')
